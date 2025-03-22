59fb0530ec7ff5b348c14aa32ba3868a8d4e894f
ManchiCoder <vibhun14@gmail.com>
2025-03-22 12:35:40 -0400

    Initial Commit

---
+++ FarmerApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FarmerApp
+//
+//  Created by vibhun naredla on 3/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FarmerApp/FarmerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FarmerAppApp.swift
+//  FarmerApp
+//
+//  Created by vibhun naredla on 3/22/25.
+//
+
+import SwiftUI
+
+@main
+struct FarmerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
